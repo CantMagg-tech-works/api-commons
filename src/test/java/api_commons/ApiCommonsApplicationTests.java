9e20b9104f9ef34ba235f244862b0833c4b7b468
AntoCant <antonio.canteros.alberto@gmail.com>
2024-10-08 20:55:35 -0300

    first commit

---
+++ src/test/java/api_commons/ApiCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package api_commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
