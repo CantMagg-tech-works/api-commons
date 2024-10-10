3721e16b86687dc43824d361f52a53d68a45cc01
AntoCant <antonio.canteros.alberto@gmail.com>
2024-10-10 14:33:37 -0300

    refactor pom.xml

--- src/main/java/api_commons/ApiCommonsApplication.java
+++ src/main/java/api_commons/ApiCommonsApplication.java
@@ -1,13 +1,8 @@
 package api_commons;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class ApiCommonsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiCommonsApplication.class, args);
-	}
+  public static void main(String[] args) {
 
+  }
 }
